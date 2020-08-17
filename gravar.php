c13bce261d7b325f4a21b81bf09914afc73204bc
Victor Rodrigues <victor.rodrigues135@gmail.com>
2020-08-17 15:27:16 -0300

    first commit

---
+++ gravar.php
@@ -0,0 +1,8 @@
+<?php
+
+if ($_POST) {
+    // Lê conteúdo do arquivo json, se existir.
+    // Converte o conteúdo do arquivo para o formato Array do PHP
+    // Adiciona o novo valor recebido no post dentro do Array PHP
+    // Grava o Array modificado para o arquivo.
+}
